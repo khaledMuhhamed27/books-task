ec25703e16cf4e7eb0801eda901c96da2fe035fe
Khaled Fetah <muhammedxaled40@gmail.com>
2023-10-19 17:59:34 +0300

    ok

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -14,4 +14,29 @@ class BookController extends Controller
         return response()->json($books);
     }
     // show function
+    public function show($id)
+    {
+        $books = Book::find($id);
+        return response()->json($books);
+    }
+    // store function
+    public function store(Request $request)
+    {
+        $books = Book::create($request->all());
+        return response()->json($books);
+    }
+    // update function
+    public function update(Request $request, $id)
+    {
+        $books = Book::find($id);
+        $books->update($request->all());
+        return response()->json($books);
+    }
+    // delete function
+    public function delete($id)
+    {
+        $books = Book::find($id);
+        $books->delete();
+        return response()->json($books);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BookController;
+use App\Models\Book;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,4 +18,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// get all data
 Route::get('/books', [BookController::class, 'index'])->name('books.index');
+// get select data
+Route::get('/books/{id}', [BookController::class, 'show'])->name('books.show');
+// create new data
+Route::post('/books', [BookController::class, 'store'])->name('books.store');
+// update data
+Route::put('/books/{id}', [BookController::class, 'update'])->name('books.update');
+// delete select data
+Route::delete('/books/{id}', [BookController::class, 'delete'])->name('books.delete');
